df77e01d73321dc40354c80abc571803b4c305db
Jone <jone@ploing.de>
2025-05-10 13:31:31 +0200

    IPv6 support, whohooooo!

--- pixelflut.py
+++ pixelflut.py
@@ -15,8 +15,15 @@ ADDRESS = (sys.argv[1], int(sys.argv[2]))
 
 
 def run_thread(name):
+    protocol = sys.argv[3] if len(sys.argv) > 3 else "v4"
+    if protocol == "v6":
+        ADDRESS = (sys.argv[1], int(sys.argv[2]), 0, 0)
+        PROTOCOL = socket.AF_INET6
+    else:
+        ADDRESS = (sys.argv[1], int(sys.argv[2]))
+        PROTOCOL = socket.AF_INET
     try:
-        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+        with socket.socket(PROTOCOL, socket.SOCK_STREAM) as s:
             s.connect(ADDRESS)
             print(f"Thread {name} initialized.")
             flood(name, s)
