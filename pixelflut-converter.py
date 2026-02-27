5d5907067beb2f91fc943b61919e9cb7c03af257
Jone <jone@ploing.de>
2026-02-27 23:43:06 +0100

    convert subimage

--- pixelflut-converter.py
+++ pixelflut-converter.py
@@ -1,8 +1,10 @@
-from PIL import Image
-import sys
-import numpy as np
 import random
+import sys
 from typing import Iterable
+
+import numpy as np
+from PIL import Image
+
 
 def all_chars_same(interable: Iterable) -> bool:
     """
@@ -11,36 +13,51 @@ def all_chars_same(interable: Iterable) -> bool:
     """
     return len(set(interable)) == 1
 
-def convert_image(image_path):
-    commands = []
 
+def convert_subimage(pixels: list[tuple[int, int, tuple[int, int, int]]]) -> list[str]:
+    commands = []
+    for x, y, color in pixels:
+        try:
+            if color[3] == 0:
+                continue
+        except IndexError:
+            pass
+        hex_color = "%02x%02x%02x" % tuple(
+            color[:3]
+        )  # https://stackoverflow.com/a/3380739
+        if all_chars_same(hex_color):
+            hex_color = hex_color[0] * 2
+        command = f"PX {x} {y} {hex_color}"
+        commands.append(command)
+    random.shuffle(commands)
+    return commands
+
+
+def convert_image(image_path):
     with Image.open(image_path) as image:
-        array = np.asarray(image) # Convert image to 3-dimensional numpy array
-    for i, child in enumerate(array):
-        for j, color in enumerate(child):
-            try:
-                if color[3] == 0:
-                    continue
-            except IndexError:
-                pass
-            hex_color = "%02x%02x%02x" % tuple(
-                color[:3]
-            )  # https://stackoverflow.com/a/3380739
-            if all_chars_same(hex_color):
-                hex_color = hex_color[0] * 2
-            command = f"PX {j} {i} {hex_color}" # Build the command
-            commands.append(command) # Save the command
-    random.shuffle(commands) # Shuffle the commands to avoid sending them from top to bottom
+        array = np.asarray(image)
+    commands = []
+    commands += convert_subimage([(x, y, tuple(array[y][x])) for y in range(
+        0, array.shape[0], 2) for x in range(0, array.shape[1], 2)])
+    commands += convert_subimage([(x, y, tuple(array[y][x])) for y in range(
+        1, array.shape[0], 2) for x in range(0, array.shape[1], 2)])
+    commands += convert_subimage([(x, y, tuple(array[y][x])) for y in range(
+        0, array.shape[0], 2) for x in range(1, array.shape[1], 2)])
+    commands += convert_subimage([(x, y, tuple(array[y][x])) for y in range(
+        1, array.shape[0], 2) for x in range(1, array.shape[1], 2)])
     for command in commands:
-        print(command) # Print the commands to stdout
+        print(command)
+
 
 def main():
     if len(sys.argv) < 2:
-        print("Usage: python pixelflut-converter.py <image_path> [<image_path> ...] > actions.txt")
+        print(
+            "Usage: python pixelflut-converter.py <image_path> [<image_path> ...] > actions.txt")
         sys.exit(1)
 
     for image_path in sys.argv[1:]:
         convert_image(image_path)
+
 
 if __name__ == "__main__":
     main()
